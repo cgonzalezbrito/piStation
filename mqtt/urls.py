6711282751a0f0272f684ae74f91e5b35cddaa20
cgonzalezbrito <cgonzalezbrt@gmail.com>
2021-07-27 15:45:50 -0500

    [ADD] toggle mqtt status button

--- mqtt/urls.py
+++ mqtt/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        #Add the following(views.py call_write_data()Allows you to send data to)
+        path("ajax/", views.toggle_mqtt_status, name="toggle_mqtt_status"),
 ]
 
--- mqtt/views.py
+++ mqtt/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Broker, Location, PhysicalVar, Topic, Field, Fields
 
@@ -17,3 +18,12 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+#Method specified by url with ajax
+def toggle_mqtt_status(req):
+    if req.method == 'GET':
+        print('VEAMOS')
+        # write_data.py write_csv()Call the method.
+        #Of the data sent by ajax"input_data"To get by specifying.
+        #write_data.write_csv(req.GET.get("input_data"))
+        return HttpResponse()
